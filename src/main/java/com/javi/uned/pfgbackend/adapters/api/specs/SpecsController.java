16386e65cf01a08bb902940e2f6feaab5c90defc
Javi <olmo.injerto.javier@gmail.com>
2022-07-16 11:00:08 +0200

    Finished implementation

--- src/main/java/com/javi/uned/pfgbackend/adapters/api/specs/SpecsController.java
+++ src/main/java/com/javi/uned/pfgbackend/adapters/api/specs/SpecsController.java
@@ -1,9 +1,7 @@
 package com.javi.uned.pfgbackend.adapters.api.specs;
 
-import com.javi.uned.melodiacore.model.specs.ScoreSpecs;
-import com.javi.uned.pfgbackend.adapters.api.specs.model.GeneticSpecsDTO;
+import com.javi.uned.melodiacore.model.specs.ScoreSpecsDTO;
 import com.javi.uned.pfgbackend.domain.sheet.model.Request;
-import com.javi.uned.pfgbackend.domain.sheet.model.Sheet;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +13,10 @@ import java.io.IOException;
 public interface SpecsController {
 
     @PostMapping(value = "/api/specs/genetic-specs", produces = MediaType.APPLICATION_JSON_VALUE)
-    Request postGeneticSpecs(@RequestBody GeneticSpecsDTO geneticSpecsDTO) throws IOException;
+    Request postGeneticSpecs(@RequestBody ScoreSpecsDTO geneticSpecsDTO) throws IOException;
 
     @GetMapping(value = "/api/specs/genetic-specs/random", produces = MediaType.APPLICATION_JSON_VALUE)
-    ScoreSpecs getRandomGeneticSpecs(
+    ScoreSpecsDTO getRandomGeneticSpecs(
             @RequestParam int requesterId);
 
 }
--- src/main/java/com/javi/uned/pfgbackend/adapters/api/specs/SpecsControllerImpl.java
+++ src/main/java/com/javi/uned/pfgbackend/adapters/api/specs/SpecsControllerImpl.java
@@ -5,14 +5,12 @@ import com.javi.uned.melodiacore.model.Instrumento;
 import com.javi.uned.melodiacore.model.constants.Figuras;
 import com.javi.uned.melodiacore.model.constants.Instrumentos;
 import com.javi.uned.melodiacore.model.specs.ScoreSpecs;
+import com.javi.uned.melodiacore.model.specs.ScoreSpecsDTO;
 import com.javi.uned.melodiacore.util.MelodiaRandom;
-import com.javi.uned.pfgbackend.adapters.api.specs.model.GeneticSpecsDTO;
-import com.javi.uned.pfgbackend.adapters.api.specs.model.GeneticSpecsDTOTransformer;
 import com.javi.uned.pfgbackend.domain.ports.filesystem.FileSystem;
 import com.javi.uned.pfgbackend.domain.ports.messagebroker.MessageBrokerGeneticComposer;
 import com.javi.uned.pfgbackend.domain.sheet.SheetService;
 import com.javi.uned.pfgbackend.domain.sheet.model.Request;
-import com.javi.uned.pfgbackend.domain.sheet.model.Sheet;
 import com.javi.uned.pfgbackend.domain.util.UtilService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -43,18 +40,15 @@ public class SpecsControllerImpl implements SpecsController {
     private UtilService utilService;
 
     @Override
-    public Request postGeneticSpecs(GeneticSpecsDTO geneticSpecsDTO) throws IOException {
+    public Request postGeneticSpecs(ScoreSpecsDTO scoreSpecsDTO) throws IOException {
 
-        logger.info("POST geneticSpecsDTO: " + geneticSpecsDTO);
-
-        // Transform to domain object
-        ScoreSpecs specs = GeneticSpecsDTOTransformer.toDomainObject(geneticSpecsDTO);
+        logger.info("POST geneticSpecsDTO: " + scoreSpecsDTO);
 
         // Create request
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(geneticSpecsDTO);
+        String json = objectMapper.writeValueAsString(scoreSpecsDTO);
         Request request = new Request(
-                geneticSpecsDTO.getRequesterId(),
+                scoreSpecsDTO.getRequesterId(),
                 LocalDateTime.now(),
                 null,
                 null,
@@ -68,20 +62,20 @@ public class SpecsControllerImpl implements SpecsController {
     }
 
     @Override
-    public ScoreSpecs getRandomGeneticSpecs(int requesterId) {
-        MelodiaRandom melodiaRandom = new MelodiaRandom();
-        ScoreSpecs result = new ScoreSpecs();
-        result.setRequesterId(requesterId);
-        result.setMovementTitle(utilService.generateRandomTitle());
-        result.setMovementNumber("1");
-        result.setAuthors(Arrays.asList("Melodía"));
-        result.setMeasures(ThreadLocalRandom.current().nextInt(30, 300));
-        result.setCompas(melodiaRandom.randomCompas());
-        result.setInstrumentos(new Instrumento[] {Instrumentos.PIANO});
-        result.setTonalidad(melodiaRandom.randomTonality());
-        result.setPhraseLength(new Random().nextBoolean()? 4 : 8);
-        result.setMinFigura(Figuras.SEMICORCHEA);
-        result.setMaxFigura(Figuras.REDONDA);
-        return result;
+    public ScoreSpecsDTO getRandomGeneticSpecs(int requesterId) {
+        MelodiaRandom melodiaRandom = new MelodiaRandom(); //TODO: Add method randomSpecs() to MelodiaRandom
+        ScoreSpecs scoreSpecs = new ScoreSpecs();
+        scoreSpecs.setRequesterId(requesterId);
+        scoreSpecs.setMovementTitle(utilService.generateRandomTitle());
+        scoreSpecs.setMovementNumber("1");
+        scoreSpecs.setAuthors(Arrays.asList("Melodía"));
+        scoreSpecs.setMeasures(ThreadLocalRandom.current().nextInt(30, 300));
+        scoreSpecs.setCompas(melodiaRandom.randomCompas());
+        scoreSpecs.setInstrumentos(new Instrumento[] {Instrumentos.PIANO});
+        scoreSpecs.setTonalidad(melodiaRandom.randomTonality());
+        scoreSpecs.setPhraseLength(new Random().nextBoolean()? 4 : 8);
+        scoreSpecs.setMinFigura(Figuras.SEMICORCHEA);
+        scoreSpecs.setMaxFigura(Figuras.REDONDA);
+        return scoreSpecs.toDTO();
     }
 }
